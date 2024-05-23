fe454eb2e96c8b2ff5e6f266aacf6b2bfa1f2be2
Amit Kumar <raghwat15@gmail.com>
2024-05-23 22:34:42 +0530

    added static files

--- challenges/views.py
+++ challenges/views.py
@@ -1,37 +1,42 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import HttpResponse, Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 
-# Create your views here.
 
 monthly_challenges = {
     "january": "This month is January!",
     "february": "This month is February!",
-    "march": "This month is March",
-    "april": "This month is March",
+    "march": "This month is March!",
+    "april": "This month is April!",
     "may": "This month is May!",
-    "june": "This month is June!"
+    "june": "This month is June!",
+    "july": "This month is July!",
+    "august": "This month is August!",
+    "September": "This month is Septmember!",
+    "October": "This month is October!",
+    "November": "This month is November!",
+    "December": None
 
 }
 
+# Create your views here.
+
 def index(request):
-    list_items = ""
     months = list(monthly_challenges.keys())
+
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
     
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
-    
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
 
 def monthly_challenges_by_numbers(request, month):
     months = list(monthly_challenges.keys())
 
     if month > len(months):
         return HttpResponseNotFound("Invalid Month!")
+    
     redirect_month = months[month -1]
     redirect_path = reverse("month-challenge", args=[redirect_month])
     return HttpResponseRedirect(redirect_path)
@@ -39,7 +44,40 @@ def monthly_challenges_by_numbers(request, month):
 def monthly_challenge(request, month):
     try:
         challenges_text = monthly_challenges[month]
-        return render(request, "challenges/challenge.html")
+        return render(request, "challenges/challenge.html", {
+            "text": challenges_text,
+            "month_name": month
+        })
     except:
-        return HttpResponseNotFound("<h1>This month is not is not Found!</h1>")
+        # respnose_data = render_to_string("404.html")
+        # return HttpResponseNotFound(respnose_data)
+        raise Http404()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
